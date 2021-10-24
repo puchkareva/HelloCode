ed229d87370408aa62af3bfa7c4aa0d7c9e5dea8
puchkareva <puchkareva@yandex.ru>
2021-10-24 19:48:41 +0200

    add Example 002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Example002_HelloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя ");
+            string username = Console.ReadLine();
+            Console.Write ("Привет, ");
+            Console.Write(username);
+        }
+    }
+}
