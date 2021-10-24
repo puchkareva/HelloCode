31209365b1005b1f84aa9d1d610f7bc43ef081a6
puchkareva <puchkareva@yandex.ru>
2021-10-24 20:38:47 +0200

    add Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Example003_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = 31;
+            int numberB = 5;
+            int result = numberA+numberB;
+            Console.WriteLine(result);
+
+        }
+    }
+}
