0d24d3e0543cd7dbbcd0f80dcfc26de88728557f
puchkareva <puchkareva@yandex.ru>
2021-10-24 19:09:38 +0200

    add Example001 Hello Console project

---
+++ Example001_HelloConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Example001_HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 Hello World! ");
+        
+        }
+    }
+}
