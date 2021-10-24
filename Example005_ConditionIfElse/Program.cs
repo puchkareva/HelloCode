6e0b8c56efd144798d8364f952c84a66e9ca38a5
puchkareva <puchkareva@yandex.ru>
2021-10-24 21:41:05 +0200

    add Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Example005_ConditionIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя пользователя: ");
+            string username = Console.ReadLine();
+
+            if(username.ToLower() == "маша")
+             {
+                 Console.WriteLine("Ура, это же МАША!");
+             }
+             else
+             {
+                 Console.WriteLine("Привет, ");
+                 Console.WriteLine(username);
+             }
+
+        }
+    }
+}
