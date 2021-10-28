1cf932bf17c1ed3382e2835a7f5908998d6835ab
puchkareva <puchkareva@yandex.ru>
2021-10-28 21:08:23 +0200

    add method2

--- Example008_Intro_Method/Program.cs
+++ Example008_Intro_Method/Program.cs
@@ -14,7 +14,7 @@ namespace Example008_Intro_Method
                 return result;
             }
             
-            int a1 = 15;
+            int a1 = 112125;
             int b1 = 21;
             int c1 = 39;
             int a2 = 12;
@@ -24,10 +24,15 @@ namespace Example008_Intro_Method
             int b3 = 23;
             int c3 = 313;
 
-            int max1 = Max(a1, b1, c1);
-            int max2 = Max(a2, b2, c2);
-            int max3 = Max(a3, b3, c3);
-            int max = Max(max1, max2, max3);
+           //  int max1 = Max(a1, b1, c1);
+           //  int max2 = Max(a2, b2, c2);
+           //  int max3 = Max(a3, b3, c3);
+           //  int max = Max(max1, max2, max3);
+
+           int max = Max(
+               Max(a1, b1, c1), 
+               Max(a2, b2, c2), 
+               Max(a3, b3, c3));
 
             Console.WriteLine(max);
 
