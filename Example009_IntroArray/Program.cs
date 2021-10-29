c65c87b1c0617f36a35cad08618d404790af54ec
puchkareva <puchkareva@yandex.ru>
2021-10-29 11:19:17 +0200

    add метод заполнения массива и печати его на экран

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -25,24 +25,52 @@ namespace Example009_IntroArray
           // );
           // Console.WriteLine(result);
 
-          int[] array = { 1, 12, 31, 18, 4, 15, 16, 17, 18 };
+          // int[] array = { 1, 12, 31, 18, 4, 15, 16, 17, 18 };
 
-          int n = array.Length;
-          int find = 18;
+          // int n = array.Length;
+          // int find = 18;
 
-          int index = 0;
+          // int index = 0;
 
-          while (index < n)
-          {
+          // while (index < n)
+          // {
 
-              if(array[index] == find)
-              {
-                  Console.WriteLine(index);
-                  break;
-              }
+             // if(array[index] == find)
+            // {
+               //   Console.WriteLine(index);
+                 // break;
+              // }
               //index = index + 1;
-              index++;
+              // index++;
+          // }
+
+          void FillArray(int[] collection)
+          {
+              int length = collection.Length;
+              int index = 0;
+              while (index < length)
+              {
+                  collection[index] = new Random().Next(1, 10);
+                  //index = index + 1;
+                  index ++;
+              }
           }
+          
+          void PrintArray(int[] col)
+          {
+              int count = col.Length;
+              int position = 0;
+              while (position < count)
+              {
+                  Console.WriteLine(col[position]);
+                  position++;
+              }
+          }
+
+          int[] array = new int[10];
+
+          FillArray(array);
+          PrintArray(array);
 
 
         }
