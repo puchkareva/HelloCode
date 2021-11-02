d74ce811c7abda52200e946a0640f79644b03df2
puchkareva <puchkareva@yandex.ru>
2021-11-02 22:03:30 +0100

    add Example 010 Methods

---
+++ Example010_Methods/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Example010_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*// Вид 1
+            void Method1()
+            {
+                Console.WriteLine("Автор ...");
+            }
+            Method1(); //способ вызова этого метода. Обязательно со скобками
+
+            // Вид 2
+            void Method2(string msg)
+            {
+                Console.WriteLine(msg);
+            }
+            Method2("Текст сообщения");
+            // или Method2(msg: "Текст сообщения");
+
+            void Method21(string msg, int count)
+            {
+                int i = 0;
+                while(i < count)
+                {
+                    Console.WriteLine(msg);
+                    i++;
+                }   
+            }
+            // Method21("Текст", 4);
+            // Method21(msg: "Текст", count: 4); 
+            Method21(count: 4, msg: "новый текст");
+
+            // Вид 3
+
+            int Method3()
+            {
+                return DateTime.Now.Year;
+            }
+            int year = Method3();
+            Console.WriteLine(year);
+
+            */
+
+            // Вид 4
+
+            int Method4(int count, string text)
+            {
+                int i = 0;
+                string result = String.Empty;
+
+                while (i < count)
+                {
+                     result = result + text;
+                     i++;
+                }
+                return result;
+            }
+            string res = Method4(10, "z");
+            Console.WriteLine(res);
+
+
+
+
+        }
+    }
+}
