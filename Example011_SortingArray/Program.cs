a96778720418fa76067bdde1ae6f20324b36716c
puchkareva <puchkareva@yandex.ru>
2021-11-03 21:32:53 +0100

    add метод упорядочевания массива

--- Example011_SortingArray/Program.cs
+++ Example011_SortingArray/Program.cs
@@ -19,7 +19,27 @@ namespace Example011_SortingArray
                 Console.WriteLine();
             }
 
+            // метод, который будет упорядочивать массив:
+            void SelectionSort (int[] array)
+            {
+                for (int i = 0; i < array.Length -1; i++)
+                {
+                    int minPosition = i;
+
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if(array[j] < array[minPosition]) minPosition = j;
+                    }
+                    int temporary = array[i];
+                    array[i] = array[minPosition];
+                    array[minPosition] = temporary;
+                }
+            }
+
             PrintArray(arr);
-        }
+            SelectionSort(arr);
+            
+            PrintArray(arr);
+       }
     }
 }
