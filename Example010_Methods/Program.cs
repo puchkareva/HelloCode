53897870541c8d47dcf7fc7ea3e0db9c46652f19
puchkareva <puchkareva@yandex.ru>
2021-11-03 16:54:12 +0100

    add цикл в цикле

--- Example010_Methods/Program.cs
+++ Example010_Methods/Program.cs
@@ -62,7 +62,7 @@ namespace Example010_Methods
             string res = Method4(10, "z");
             Console.WriteLine(res);
 
-            */
+            
 
             string Method4(int count, string text)
             {
@@ -76,7 +76,16 @@ namespace Example010_Methods
             string res = Method4(10, "z");
             Console.WriteLine(res);
 
+            */
 
+            for(int i = 2; i <= 10; i++)
+            {
+                for(int j = 2; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i} x {j} = {i * j}");
+                }
+                Console.WriteLine();
+            }
 
 
 
