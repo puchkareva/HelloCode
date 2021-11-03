f52efe5bbf5a56e8db8f2d05f60a523d40933b40
puchkareva <puchkareva@yandex.ru>
2021-11-03 17:59:36 +0100

    add Example 004 Div_1

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -6,7 +6,9 @@ namespace Example004_Div
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double numberA = 12;
+            double numberB = 5;
+            Console.WriteLine(numberA / numberB);
         }
     }
 }
