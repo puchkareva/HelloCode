659939abf0251bc8a7666f04fe50e70fa2d3c37a
puchkareva <puchkareva@yandex.ru>
2021-11-03 21:22:41 +0100

    add Example 011 Sorting Array

---
+++ Example011_SortingArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Example011_SortingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+            
+            void PrintArray(int[] array)
+            {
+                int count = array.Length;
+
+                for (int i = 0; i < count; i++)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+                Console.WriteLine();
+            }
+
+            PrintArray(arr);
+        }
+    }
+}
